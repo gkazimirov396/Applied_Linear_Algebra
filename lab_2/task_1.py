e7833a0183f096ff817bdda7946481e1e97bcccf
gkazimirov396 <grishakazim@gmail.com>
2024-06-16 19:59:48 +0300

    feat: task 1 finished

---
+++ lab_2/task_1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def eigenvalues_and_eigenvectors(matrix):
+  eigenvalues, eigenvectors = np.linalg.eig(matrix)
+
+  return eigenvalues, eigenvectors
+
+def check_eigen(matrix, eigenvalues, eigenvectors):
+    for i in range(len(eigenvalues)):
+      v = eigenvectors[:, i];
+      lv = eigenvalues[i] * v;
+      Av = np.dot(matrix, v);
+
+      if not np.allclose(Av, lv):
+          return False
+        
+    return True
+
+# Example 
+A = np.array([[7, -2], [1,  -1]])
+
+eigenvalues, eigenvectors = eigenvalues_and_eigenvectors(A)
+
+print("Eigenvalues:", eigenvalues)
+print("Eigenvectors:\n", eigenvectors)
+
+is_equal = check_eigen(A, eigenvalues, eigenvectors)
+print(is_equal)
